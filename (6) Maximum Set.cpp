d51e0224a9837c71f5bf36fdb9ab72d09716307f
Zahid Hasan Mozumder <73828619+Zahid-Hasan-Mozumder@users.noreply.github.com>
2023-04-18 18:42:03 +0600

    Create (6) Maximum Set.cpp

---
+++ (6) Maximum Set.cpp
@@ -0,0 +1,84 @@
+// Problem link: https://codeforces.com/problemset/problem/1796/C
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#include <bits/stdc++.h>
+#include <climits>
+
+using namespace std;
+
+#define ll long long int
+
+#define pii pair<int, int>
+#define pli pair<ll, int>
+#define pil pair<int, ll>
+#define pll pair<ll, ll>
+#define psi pair<string, int>
+#define psl pair<string, ll>
+#define piii pair<pair<int, int>, int>
+#define pipii pair<int, pair<int, int>>
+#define piipii pair<pair<int, int>, pair<int, int>>
+#define vi vector<int>
+#define vll vector<ll>
+#define vb vector<bool>
+#define vvi vector<vector<int>>
+#define vvll vector<vector<ll>>
+#define vvb vector<vector<bool>>
+#define vs vector<string>
+#define vpii vector<pair<int, int>>
+#define vpll vector<pair<ll, ll>>
+#define vpli vector<pair<ll, int>>
+#define vpil vector<pair<int, ll>>
+#define vpipii vector<pair<int, pair<int, int>>>
+#define vvpii vector<vector<pair<int, int>>>
+#define vvpll vector<vector<pair<ll, ll>>>
+#define plpii pair<ll, pair<int, int>>
+#define vplpii vector<pair<ll, pair<int, int>>>
+#define vvpil vector<vector<pair<int, ll>>>
+#define pb push_back
+#define pf push_front
+#define ff first
+#define ss second
+
+const int mod = 1e9+7;
+const int MOD = 998244353;
+const int maxN = 5e6+5;
+const int sz = 1e6+2;
+const double pi = acos(-1.0);
+const double PI = 3.1415926535897;
+
+void zahid(){
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("Error.txt", "w", stderr);
+    #endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+
+int main(){
+
+    zahid();
+    
+    int t; cin >> t;
+    while(t--){
+        ll l, r; cin >> l >> r;
+        ll len = 0;
+        while(l * pow(2, len) <= r){
+            len++;
+        }
+        if(len == 1){
+            ll ways = r - l + 1;
+            cout<< len << " " << ways << '\n';
+        }
+        else{
+            ll ways = max(0LL, (ll)(r / pow(2, len - 1)) - l + 1);
+            ways %= MOD;
+            if(len >= 2){
+                ways += (max(0LL, (ll)(r / (pow(2, len - 2) * 3)) - l + 1) * (len - 1)) % MOD;
+                ways %= MOD;
+            }
+            cout<< len << " " << ways << '\n';
+        }
+    }
+}
